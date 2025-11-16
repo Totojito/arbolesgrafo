58668ac15ea156dde860b1f51344fe9e34380453
Totojito <axelalgaba7@gmail.com>
2025-11-16 17:00:04 -0600

    anadi funciones -Insertar -Buscar -Recorrer -ContarNodos
    anadi los botones mostrar, contar y textbox

--- arbolesgrafo/Form1.Designer.cs
+++ arbolesgrafo/Form1.Designer.cs
@@ -28,21 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnMostrarArbol = new System.Windows.Forms.Button();
+            this.txtArbolResultado = new System.Windows.Forms.TextBox();
+            this.btnContar = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // btnMostrarArbol
+            // 
+            this.btnMostrarArbol.Location = new System.Drawing.Point(115, 63);
+            this.btnMostrarArbol.Name = "btnMostrarArbol";
+            this.btnMostrarArbol.Size = new System.Drawing.Size(75, 23);
+            this.btnMostrarArbol.TabIndex = 0;
+            this.btnMostrarArbol.Text = "Mostrar";
+            this.btnMostrarArbol.UseVisualStyleBackColor = true;
+            this.btnMostrarArbol.Click += new System.EventHandler(this.btnMostrarArbol_Click);
+            // 
+            // txtArbolResultado
+            // 
+            this.txtArbolResultado.Location = new System.Drawing.Point(196, 63);
+            this.txtArbolResultado.Multiline = true;
+            this.txtArbolResultado.Name = "txtArbolResultado";
+            this.txtArbolResultado.Size = new System.Drawing.Size(100, 22);
+            this.txtArbolResultado.TabIndex = 1;
+            // 
+            // btnContar
+            // 
+            this.btnContar.Location = new System.Drawing.Point(115, 106);
+            this.btnContar.Name = "btnContar";
+            this.btnContar.Size = new System.Drawing.Size(75, 23);
+            this.btnContar.TabIndex = 2;
+            this.btnContar.Text = "Contar";
+            this.btnContar.UseVisualStyleBackColor = true;
+            this.btnContar.Click += new System.EventHandler(this.btnContar_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(467, 487);
+            this.Controls.Add(this.btnContar);
+            this.Controls.Add(this.txtArbolResultado);
+            this.Controls.Add(this.btnMostrarArbol);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnMostrarArbol;
+        private System.Windows.Forms.TextBox txtArbolResultado;
+        private System.Windows.Forms.Button btnContar;
     }
 }
 
--- arbolesgrafo/Form1.cs
+++ arbolesgrafo/Form1.cs
@@ -7,9 +7,81 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using arbolesgrafo;
 
 namespace arbolesgrafo
 {
+    public partial class Form1 : Form
+    {
+      
+        private Arbol arbolJerarquia;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+           
+            Nodo raiz = new Nodo("Rectoría");
+
+           
+            arbolJerarquia = new Arbol(raiz);
+
+           
+            arbolJerarquia.Insertar("Rectoría", "Vicerrectoría Académica");
+            arbolJerarquia.Insertar("Rectoría", "Vicerrectoría Administrativa");
+
+            arbolJerarquia.Insertar("Vicerrectoría Académica", "Facultad de Ingeniería");
+            arbolJerarquia.Insertar("Vicerrectoría Académica", "Facultad de Derecho");
+
+            arbolJerarquia.Insertar("Facultad de Ingeniería", "Departamento de Sistemas");
+            arbolJerarquia.Insertar("Facultad de Ingeniería", "Departamento Civil");
+
+           
+            Nodo nodoBuscado = arbolJerarquia.Buscar("Facultad de Ingeniería");
+            if (nodoBuscado != null)
+            {
+                
+                Console.WriteLine("¡Nodo encontrado!");
+            }
+        }
+
+        
+        private void btnMostrarArbol_Click(object sender, EventArgs e)
+        {
+            
+            if (arbolJerarquia == null)
+            {
+                MessageBox.Show("El árbol aún no ha sido inicializado.");
+                return;
+            }
+
+            
+            string textoDelArbol = arbolJerarquia.RecorrerPreOrden();
+
+            
+            txtArbolResultado.Text = textoDelArbol;
+        }
+
+        
+        private void btnContar_Click(object sender, EventArgs e)
+        {
+            if (arbolJerarquia == null)
+            {
+                MessageBox.Show("El árbol aún no ha sido inicializado.");
+                return;
+            }
+
+            
+            int totalNodos = arbolJerarquia.ContarNodos();
+
+            MessageBox.Show($"El número total de nodos (puestos/deptos) es: {totalNodos}");
+        }
+    }
 
     public class Nodo
     {
@@ -32,6 +104,7 @@ namespace arbolesgrafo
     }
 
 
+
     public class Arbol
     {
 
@@ -43,19 +116,147 @@ namespace arbolesgrafo
             this.Raiz = raiz;
         }
 
+        // --- Aquí pondremos las funciones principales ---
 
-    }
-
-    public partial class Form1 : Form
-    {
-        public Form1()
+        /// <summary>
+        /// Busca un nodo en el árbol basado en su valor (nombre).
+        /// Utiliza Búsqueda en Amplitud (BFS).
+        /// </summary>
+        /// <param name="valorBuscado">El string (ej. "Rector") que queremos encontrar.</param>
+        /// <returns>El Nodo si se encuentra, o null si no existe.</returns>
+        public Nodo Buscar(string valorBuscado)
         {
-            InitializeComponent();
+
+            if (this.Raiz == null)
+            {
+                return null;
+            }
+
+
+            Queue<Nodo> cola = new Queue<Nodo>();
+
+
+            cola.Enqueue(this.Raiz);
+
+
+            while (cola.Count > 0)
+            {
+
+                Nodo nodoActual = cola.Dequeue();
+
+
+                if (nodoActual.Valor == valorBuscado)
+                {
+                    return nodoActual;
+                }
+
+
+                foreach (Nodo hijo in nodoActual.Hijos)
+                {
+                    cola.Enqueue(hijo);
+                }
+            }
+
+
+            return null;
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+
+        /// <summary>
+        /// Inserta un nuevo nodo en el árbol.
+        /// </summary>
+        /// <param name="valorPadre">El valor (nombre) del nodo al que queremos agregar el hijo.</param>
+        /// <param name="valorNuevo">El valor (nombre) del nuevo nodo a crear.</param>
+        /// <returns>True si se pudo insertar, False si el padre no existe.</returns>
+        public bool Insertar(string valorPadre, string valorNuevo)
         {
 
+            Nodo nodoPadre = this.Buscar(valorPadre);
+
+
+            if (nodoPadre == null)
+            {
+
+                return false;
+            }
+
+
+            Nodo nuevoNodo = new Nodo(valorNuevo);
+
+
+            nuevoNodo.Padre = nodoPadre;
+
+
+            nodoPadre.Hijos.Add(nuevoNodo);
+
+            return true;
+        }
+
+       
+        public string RecorrerPreOrden()
+        {
+           
+            StringBuilder sb = new StringBuilder();
+
+            
+            RecorrerPreOrden_Recursivo(this.Raiz, 0, sb);
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Función PRIVADA recursiva que hace el trabajo de recorrer.
+        /// </summary>
+        /// <param name="nodo">El nodo que estamos visitando ahora.</param>
+        /// <param name="nivel">La profundidad (para saber cuánta sangría poner).</param>
+        /// <param name="sb">El StringBuilder donde vamos armando el texto.</param>
+        private void RecorrerPreOrden_Recursivo(Nodo nodo, int nivel, StringBuilder sb)
+        {
+            
+            if (nodo == null) return;
+
+            
+            sb.Append(new string(' ', nivel * 4)); 
+            sb.AppendLine(nodo.Valor); 
+
+            
+            foreach (Nodo hijo in nodo.Hijos)
+            {
+               
+                RecorrerPreOrden_Recursivo(hijo, nivel + 1, sb);
+            }
+        }
+        // ... (Aquí están tus funciones de Recorrer) ...
+
+        /// <summary>
+        /// Función PÚBLICA que inicia el conteo de nodos.
+        /// </summary>
+        /// <returns>El número total de nodos en el árbol.</returns>
+        public int ContarNodos()
+        {
+            
+            return ContarNodos_Recursivo(this.Raiz);
+        }
+
+        
+        private int ContarNodos_Recursivo(Nodo nodo)
+        {
+            
+            if (nodo == null)
+            {
+                return 0;
+            }
+
+            
+            int conteo = 1;
+
+           
+            foreach (Nodo hijo in nodo.Hijos)
+            {
+                conteo += ContarNodos_Recursivo(hijo);
+            }
+
+            return conteo;
         }
     }
 }
